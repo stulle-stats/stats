bfabf5a506c5945e840a842afbce0fda772ac1c0
stulle-stats <261638087+stulle-stats@users.noreply.github.com>
2026-02-14 23:08:43 +0100

    Update site

--- scripts/build_manifesto.R
+++ scripts/build_manifesto.R
@@ -1,7 +1,7 @@
-setwd("C:/Users/t/Documents/Statsspielerei/data")
+
 
 library(tidyverse)
 
-manifesto_raw <- read_csv("raw/Politics/Manifesto Project/MPDataset_MPDS2025a.csv")
+manifesto_raw <- read_csv("data/raw/Politics/Manifesto Project/MPDataset_MPDS2025a.csv")
 
 
